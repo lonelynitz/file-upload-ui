45aaefb7800c9cdd354bfcafb5c003a79508f77b
lonelynitz <vasanthmurugesan47@gmail.com>
2026-06-03 14:13:03 +0530

    fix: implement lazy loading for FileUpload component and update CSS styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
-import { FileUpload } from './fileUpload'
+import { Suspense, lazy } from 'react'
+
+const FileUpload = lazy(() => import('./fileUpload').then(module => ({ default: module.FileUpload })))
 
 function App() {
   return (
-    <div style={{ "textAlign" : "center" }}>
-      <FileUpload/>
+    <div style={{ textAlign: 'center' }}>
+      <Suspense fallback={<p>Loading file upload...</p>}>
+        <FileUpload />
+      </Suspense>
     </div>
   )
 }
